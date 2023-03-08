1fab3e79f7fbb9f48d92f86cdd61a53f50326944
koddo <2563454+koddo@users.noreply.github.com>
2023-03-09 01:03:19 +0300

    Add commented out lines for trying out (clojure.java.shell/sh "...")

--- src/core.clj
+++ src/core.clj
@@ -1,9 +1,16 @@
 (ns core
   "https://veson.dev/blog -- statically generated"
   (:require [stasis.core :as stasis])
+  (:require [clojure.java.shell])
   )
 
 (stasis/slurp-directory "blog" #".*\.org$")
+
+(comment
+  (clojure.java.shell/sh "ls asdf")
+  (clojure.java.shell/sh "nix-shell" "--run" "cat blog/test-entry.org | ./build.el")
+  (clojure.java.shell/sh "nix-shell" "--run" "cat blog/test-entry.org | ./build.el | tidy --indent auto --show-body-only yes --quiet yes")
+  )
 
 (defn exec
   "Invoke me with clojure -X core/exec"
