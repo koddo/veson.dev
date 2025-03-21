a6aec81d28d5a01a83dd99e9f2158124b812049d
Alejandro Veson <alejandro@veson.dev>
2025-03-21 17:12:31 +0300

    feat: remove the about and the feed pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -103,10 +103,6 @@ export default async function(eleventyConfig) {
 		outputPath: "/feed/feed.xml",
 		stylesheet: "pretty-atom-feed.xsl",
 		templateData: {
-			eleventyNavigation: {
-				key: "Feed",
-				order: 4
-			}
 		},
 		collection: {
 			name: "posts",
