17c0ec04be42d11cd469db6b3918ce3e7324c459
Alejandro Veson <alejandro@veson.dev>
2025-05-25 21:51:39 +0300

    feat(config): turn off 11ty image optimization
    My images get processed, I want them to be on my pages as they are.

--- eleventy.config.js
+++ eleventy.config.js
@@ -119,23 +119,6 @@ export default async function(eleventyConfig) {
 		}
 	});
 
-	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-		// File extensions to process in _site folder
-		extensions: "html",
-
-		// Output formats for each image.
-		formats: ["avif", "webp", "auto"],
-
-		// widths: ["auto"],
-
-		defaultAttributes: {
-			// e.g. <img loading decoding> assigned on the HTML tag will override these values.
-			loading: "lazy",
-			decoding: "async",
-		}
-	});
-
 	// Filters
 	eleventyConfig.addPlugin(pluginFilters);
 
